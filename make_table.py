39aa45554fec6f859d980106e3f376cf42ea8f0a
Alexander O'Donovan-Jones <alexodonovanjones@gmail.com>
2017-06-13 12:25:06 +0000

    Add user creation and ownership to the script

--- make_table.py
+++ make_table.py
@@ -1,3 +1,19 @@
+"""
+Database table and user creation script.
+This script creates a basic table for storing scraped job items, and a user to manage it. Once
+the script is executed items can be inserted, deleted, etc from the created table, with the new
+user.
+
+To use simply call the script with three arguments:
+
+    -c | --credentials  the connection string to the PostgreSQL database instance for example:
+                        "postgresql://user:pass@host:port/database". The username and password
+                        should be those of a user with the CREATETABLE and CREATEROLE roles.
+    -u | --username     the username of the to-be-created user.
+    -p | --password     the password of the to-be-created user.
+
+"""
+
 import argparse
 import datetime
 
@@ -11,7 +27,7 @@ Base = declarative_base()
 
 
 class JobsTable(Base):
-    __tablename__ = 'jobs'
+    __tablename__ = 'scraped-jobs'
     id = Column(UUID, server_default=func.gen_random_uuid(), primary_key=True)
     url = Column(Text, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
@@ -19,13 +35,36 @@ class JobsTable(Base):
     data = Column(JSONB, nullable=False)
 
 
+def install_pgcrypto(db_connection):
+    db_connection.execute("""CREATE EXTENSION IF NOT EXISTS pgcrypto;""")
+
+
+def create_table(db_connection):
+    Base.metadata.create_all(db_connection, checkfirst=True)
+
+
+def create_user(username, password, db_connection):
+    query = """CREATE USER {user} WITH PASSWORD {password!r} NOINHERIT;""".format(user=username, password=password)
+    db_connection.execute(query)
+
+
+def alter_table_owner(username, table, db_session):
+    query = """ALTER TABLE "{table}" OWNER TO {user};""".format(table=table, user=username)
+    db_session.execute(query)
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Table maker thing!')
+    parser = argparse.ArgumentParser(description='Database table and user tools')
     parser.add_argument('-c', '--credentials', dest='credentials')
+    parser.add_argument('-u', '--username', dest='username')
+    parser.add_argument('-p', '--password', dest='password')
     arguments = parser.parse_args()
 
-    engine = create_engine(arguments.credentials)
+    engine = create_engine(arguments.credentials, echo=True)
     session = sessionmaker(bind=engine)()
-    # we need to install the pgcrypto extension to make use of the `gen_random_uuid` function
-    session.bind.execute('CREATE EXTENSION IF NOT EXISTS pgcrypto;')
-    Base.metadata.create_all(session.bind, checkfirst=True)
+    connection = session.bind
+
+    install_pgcrypto(connection)
+    create_table(connection)
+    create_user(arguments.username, arguments.password, connection)
+    alter_table_owner(arguments.username, JobsTable.__tablename__, connection)
